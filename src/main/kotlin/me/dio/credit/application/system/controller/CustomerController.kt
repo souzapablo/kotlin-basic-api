219a78e1179fbc7fd621c837a265a384f558729d
Pablo Souza <oliveirasouzapablo@gmail.com>
2023-04-01 15:33:47 -0300

    feat: delete and get customer

--- src/main/kotlin/me/dio/credit/application/system/controller/CustomerController.kt
+++ src/main/kotlin/me/dio/credit/application/system/controller/CustomerController.kt
@@ -1,11 +1,11 @@
 package me.dio.credit.application.system.controller
 
+import com.electronwill.nightconfig.core.conversion.Path
 import me.dio.credit.application.system.dto.CustomerDto
+import me.dio.credit.application.system.dto.CustomerView
+import me.dio.credit.application.system.entity.Customer
 import me.dio.credit.application.system.service.impl.CustomerService
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api/v1/customers")
@@ -17,4 +17,13 @@ class CustomerController(
         val savedCustomer = this.customerService.save(customerDto.toEntity())
         return "Customer ${savedCustomer.email} saved!"
     }
+
+    @GetMapping("/{id}")
+    fun findById(@PathVariable id: Long): CustomerView {
+        val customer: Customer = this.customerService.findById(id)
+        return CustomerView(customer)
+    }
+
+    @DeleteMapping("/{id}")
+    fun deleteCustomer(@PathVariable id: Long) = this.customerService.delete(id)
 }
